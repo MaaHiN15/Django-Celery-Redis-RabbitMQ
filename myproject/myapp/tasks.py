36740df1ea57d4df28c89b32c3a804181f099750
MaaHiN15 <mahendranmaahin007@gmail.com>
2023-10-09 17:53:30 +0530

    some changes

--- myproject/myapp/tasks.py
+++ myproject/myapp/tasks.py
@@ -8,19 +8,44 @@ def first_task():
     return
 
 @shared_task
-def second_task():
+def second_task(task_defualt_rate_limit='1/m'):
     return
 
 
 @shared_task(priority=0, queue='high_priority')
 def h():
+    time.sleep(3)
     return
 
 
 @shared_task(priority=5, queue='medium_priority')
 def m():
+    time.sleep(3)
     return
 
 @shared_task(priority=10, queue='low_priority')
 def l():
+    time.sleep(3)
     return
+
+
+
+
+from celery import group
+
+# Here s() method stats signature of the task function
+task_group = group(h.s(), m.s(), l.s()) 
+
+# Here apply_async function runs the task function in async order
+task_group.apply_async()
+
+
+
+
+from celery import chain
+
+# Here s() method stats signature of the task function
+task_group = chain(h.s(), m.s(), l.s()) 
+
+# Here apply_async function runs the task function in async order
+task_group.apply_async()
--- myproject/myproject/celery.py
+++ myproject/myproject/celery.py
@@ -6,4 +6,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 celery = Celery('myproject')
 celery.config_from_object('django.conf:settings', namespace='CELERY')
 
+celery.conf.task_default_rate_limit = '1/m'
+
 celery.autodiscover_tasks()
